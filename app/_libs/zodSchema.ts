4806fb71c6a11b60e623a3c239266c5cac1e137d
KEENTO <handball0321@gmail.com>
2025-01-04 18:00:06 +0900

    Fix: error message of schema

--- app/_libs/zodSchema.ts
+++ app/_libs/zodSchema.ts
@@ -1,20 +1,12 @@
 import { z } from "zod";
 
-export const todoSchema = z
-  .object({
-    title: z
-      .string({ message: "Title is required" })
-      .trim()
-      .min(1, { message: "Title is required" })
-      .max(100, { message: "Title is too long" }),
-    isCompleted: z.union([z.literal("true"), z.literal("false")], {
-      message: "isCompleted is required",
-    }),
-  })
-  .refine(
-    (data) => data.isCompleted === "true" || data.isCompleted === "false",
-    {
-      message: "isCompleted is required",
-      path: ["isCompleted"],
-    }
-  );
+export const todoSchema = z.object({
+  title: z
+    .string({ message: "Title is required" })
+    .trim()
+    .min(1, { message: "Title is required" })
+    .max(100, { message: "Title is too long" }),
+  isCompleted: z.union([z.literal("true"), z.literal("false")], {
+    errorMap: () => ({ message: "isCompleted is required" }),
+  }),
+});
