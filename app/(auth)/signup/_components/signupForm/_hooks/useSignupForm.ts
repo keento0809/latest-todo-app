f38108fc8fdc92f7a4731635ea29b370ac27645b
KEENTO <handball0321@gmail.com>
2025-01-05 15:51:30 +0900

    Fix: signup action

--- app/(auth)/signup/_components/signupForm/_hooks/useSignupForm.ts
+++ app/(auth)/signup/_components/signupForm/_hooks/useSignupForm.ts
@@ -2,14 +2,43 @@ import { signup } from "@/app/_features/(auth)/signup/actions";
 import { signupSchema } from "@/app/_libs/zodSchema";
 import { useForm } from "@conform-to/react";
 import { parseWithZod } from "@conform-to/zod";
+import { signIn } from "next-auth/react";
 import { useActionState } from "react";
+
+type SignupResultType = {
+  isSuccess: boolean;
+  data: {
+    email: string | null;
+    password: string;
+  };
+};
+
+function checkIsSignupResultType(result: unknown): result is SignupResultType {
+  return (result as SignupResultType).data !== undefined;
+}
 
 export const useSignupForm = () => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [result, action, isPending] = useActionState(
     async (_: unknown, formData: FormData) => {
-      await signup({ formData });
-      return;
+      const signupResult = await signup({ formData });
+
+      if (signupResult.error) {
+        throw new Error(`Failed to signup: ${signupResult.error.message}`);
+      }
+
+      // ensure the result is of the correct type
+      const isSignupResultType = checkIsSignupResultType(signupResult);
+
+      if (!isSignupResultType) {
+        throw new Error("Invalid signup result type");
+      }
+
+      await signIn("credentials", {
+        email: signupResult.data.email,
+        password: signupResult.data.password,
+        redirectTo: "/",
+      });
     },
     null
   );
