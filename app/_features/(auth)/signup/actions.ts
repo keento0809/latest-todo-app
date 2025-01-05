fbf94a023552cd9c66f3e8721b3bca1b8a878f38
KEENTO <handball0321@gmail.com>
2025-01-05 15:40:20 +0900

    Add: functionalities to signup action

--- app/_features/(auth)/signup/actions.ts
+++ app/_features/(auth)/signup/actions.ts
@@ -1,5 +1,9 @@
+import { db } from "@/app/_db/drizzle";
+import { users } from "@/app/_db/schema";
 import { signupSchema } from "@/app/_libs/zodSchema";
 import { parseWithZod } from "@conform-to/zod";
+import { eq } from "drizzle-orm";
+import bcrypt from "bcryptjs";
 
 export async function signup({ formData }: { formData: FormData }) {
   const result = parseWithZod(formData, { schema: signupSchema });
@@ -7,4 +11,34 @@ export async function signup({ formData }: { formData: FormData }) {
   if (result.status !== "success") {
     return result.reply();
   }
+
+  // check if user already exists
+  const existingUser = await db.query.users.findFirst({
+    where: eq(users.email, result.value.email),
+  });
+
+  if (existingUser) {
+    return {
+      isSuccess: false,
+      error: { message: "User already exists" },
+    };
+  }
+
+  // hash password
+  const hashedPassword = await bcrypt.hash(result.value.password, 12);
+
+  const [newUser] = await db
+    .insert(users)
+    .values({
+      name: result.value.username,
+      email: result.value.email,
+      hashedPassword,
+      image: "",
+    })
+    .returning();
+
+  return {
+    isSuccess: true,
+    data: { email: newUser.email, password: result.value.password },
+  };
 }
