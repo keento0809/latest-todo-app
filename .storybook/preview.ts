4e3ef9c0c44970357b1a50a713c068a4c7d27c3a
KEENTO <handball0321@gmail.com>
2025-05-07 20:16:48 +0900

    Add: Configuration for TailwindCSS

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,11 +1,16 @@
-import type { Preview } from '@storybook/react'
+import type { Preview } from "@storybook/react";
+import "../app/globals.css";
+
+// export const parameters = {
+//   actions: { argTypesRegex: "^on[A-Z].*" },
+// };
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
