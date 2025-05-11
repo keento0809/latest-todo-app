c629cb8786a6b1e829c4ee774809720f2d99d996
KEENTO <handball0321@gmail.com>
2025-05-11 17:49:38 +0900

    Fix: Moved logic to HomeContainer.tsx

--- app/(home)/HomePresentation.tsx
+++ app/(home)/HomePresentation.tsx
@@ -2,18 +2,23 @@
 
 import { TodoForm } from "./_components/todoForm/TodoForm";
 import { TodoList } from "./_components/todoList/TodoList";
-import { useHome } from "./_hooks/useHome";
-import { Todo } from "@/app/(home)/_types/home";
+import { TodoFields, TodoFormType, TodoObj } from "@/app/(home)/_types/home";
 
-type HomePresentationProps = {
-  todos: Todo[];
+export type HomePresentationProps = {
+  todoState: TodoObj;
+  setStateAction: (payload: FormData) => void;
+  isPending: boolean;
+  form: TodoFormType;
+  fields: TodoFields;
 };
 
-export const HomePresentation = ({ todos }: HomePresentationProps) => {
-  const { todoState, setStateAction, isPending, form, fields } = useHome({
-    todos,
-  });
-
+export const HomePresentation = ({
+  todoState,
+  setStateAction,
+  isPending,
+  form,
+  fields,
+}: HomePresentationProps) => {
   return (
     <div className="flex flex-col gap-16">
       <TodoForm
